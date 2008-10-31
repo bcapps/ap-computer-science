ac66960df91e20bde8eed6a0a8ec289ff2369fb0
Brian <bcapps322@gmail.com>
2008-10-31 02:44:48 -0400

    monopoly - fundamentals sound, needs odds+ends

--- projects/monopoly_v1/Monopoly.java
+++ projects/monopoly_v1/Monopoly.java
@@ -7,27 +7,69 @@ import java.util.Scanner;
 
 public class Monopoly
 {
+	private static Player brian;
+	
 	public static void main (String [] args)
 	{
 		Scanner in = new Scanner (System.in);
-
-		Die d1 = new Die(6);
-		Die d2 = new Die(6);
 		
 		System.out.println(welcome());
 		System.out.print("\n\nPlease enter your name: ");
+		brian = new Player(in.next());
+		System.out.println("Welcome, "+brian.getName()+"!");
 		
-		Player brian = new Player(in.next());
-		System.out.print("Enter 1 to roll or 2 to exit: ");
-		//try{
+		int status = 3;
+		boolean exit = false;
+		
+		while (!exit)
+		{
+			while(status !=1 && status !=2)
+			{
+				System.out.print("\nEnter 1 to roll or 2 to exit: ");
+				status = in.nextInt();
+			}//end while status
 			
-		//}
-		
-		
+			if (status ==1)
+				play();
+			else if (status == 2)
+			{
+				System.out.println("\nGoodbye!");
+				System.out.println("You left Monopoly on space "+brian.getSpace()+
+									" with $"+brian.getMoney()+".");
+				System.out.println("Thanks for playing, bud.");
+				exit = true;
+			}
+			
+			status = 3;//resets status to unkown
+		}//end while exit
 	}//end main
+	public static void play()
+	{
+		Die d1 = new Die(6);
+		Die d2 = new Die(6);
+		int r1 = d1.nextRoll();
+		int r2 = d2.nextRoll();
+		int rolled = (r1+r2);
+		
+		System.out.println("\nYou rolled a "+r1+" and a "+r2+", for a total of "+rolled);
+		
+		brian.movePiece(rolled);
+		if (brian.getSpace() >= 40)
+		{
+			brian.setMoney(200);
+			brian.setSpace((brian.getSpace() - 40));
+			System.out.println("You received $200 for passing GO.");
+		}
+		else
+			System.out.println("You did not pass GO.");
+			
+		System.out.println("You currently have $"+brian.getMoney());	
+		System.out.println("You are on space "+brian.getSpace());
+		
+	}//end play method
 	public static String welcome()
 	{
-		return "                                                                    *---.*              \n"+        
+		return "                                                                                        \n"+        
 "	                                        *-:|ossttt||||:-*              :otoooo-                     \n"+
 "	                                      oo|hmmmmN-*****smmdh|           .sts-:tt.                     \n"+
 "	                                      m: ommmmN|    *dmmmmo           *sto|*                        \n"+
@@ -69,5 +111,5 @@ public class Monopoly
 "	                           *-:tosssssotth               doosssoot|:-.                               \n"+
 "	                    *-.**********..****-y               h*****.:--.*******..*                       \n"+
 "	                     *-||ttoott|:. -tttt:               |ttt|-  .:||||:::-.";
-	}
+	}//end snazzy welcome method
 }//end class
