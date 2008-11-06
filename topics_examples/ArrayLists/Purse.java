0cc70b0bcb3bdc4b60180cec63176da468bfc2bd
Students - bcapps <bcapps322@gmail.com>
2008-11-06 10:04:10 -0500

    Purse/Coins work

--- topics_examples/ArrayLists/Purse.java
+++ topics_examples/ArrayLists/Purse.java
@@ -18,11 +18,24 @@ public class Purse
 	{
 		coins = (ArrayList<Coin>) c.clone();
 	}
-	
 	public void addCoin(Coin n)
 	{
 		coins.add(n);
 	}
+	public boolean sameContents(Purse otherPurse)
+	{
+		if(coins.size()!= otherPurse.coins.size())
+			return false;
+		for(int i= coins.size()-1; i>=0; i--)
+			if(coins.get(i).getName()!=otherPurse.coins.get(i).getName() ||
+				coins.get(i).getValue()!=otherPurse.coins.get(i).getValue())
+					return false;
+		return true;
+	}
+	//public boolean sameCoins(Purse otherPurse)
+	//{
+		
+	//}
 	public void reverse()
 	{
 		ArrayList<Coin> temp = new ArrayList<Coin>();
@@ -30,7 +43,12 @@ public class Purse
 			temp.add(coins.remove(i));
 		coins = (ArrayList<Coin>) temp.clone();
 	}
-	
+	public void transfer(Purse otherPurse)
+	{
+		for(int i =0; i<otherPurse.coins.size(); i++)
+			coins.add(otherPurse.coins.get(i));
+		otherPurse.coins = new ArrayList<Coin>();
+	}
 	public String toString()
 	{
 		String result = "Purse contents: [ ";
