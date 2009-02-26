38fc67f8ff0dbd4076d1cdc898863e0b0c962d2b
Students - bcapps <bcapps322@gmail.com>
2009-02-26 09:31:54 -0500

    lab9 - selSort finished

--- labs/lab9/linsearch/LinearSearchDemo.java
+++ labs/lab9/linsearch/LinearSearchDemo.java
@@ -2,37 +2,30 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 /**
-   This program demonstrates the binary search algorithm.
+   This program demonstrates the linear search algorithm.
 */
-public class BinarySearchDemo
+public class LinearSearchDemo
 {  
    public static void main(String[] args)
    {  
-		StopWatch t = new StopWatch();
-		t.reset();
       // Construct random array
    
-      int[] a = ArrayUtil.randomIntArray(10000, 100000);
-      Arrays.sort(a);
+      int[] a = ArrayUtil.randomIntArray(20, 100);
       System.out.println(Arrays.toString(a));
-      BinarySearcher searcher = new BinarySearcher(a);
+      LinearSearcher searcher = new LinearSearcher(a);
+
       Scanner in = new Scanner(System.in);
 
       boolean done = false;
       while (!done)
       {
-         System.out.print
-               ("Enter number to search for, -1 to quit:");
+         System.out.print("Enter number to search for, -1 to quit: ");
          int n = in.nextInt();
          if (n == -1) 
             done = true;
          else
          {
-			t.start();
             int pos = searcher.search(n);
-			t.stop();
-			System.out.println("Search took "+t.getElapsedTime()+" nanosecondss");
-			
             System.out.println("Found in position " + pos);
          }
       }
